77ee2101eda9eae2e1822d940d12540169e1d727
Shirshov Artyom <artyomsh01@yandex.ru>
2021-09-01 16:45:02 +0300

    + a lot of spaces

--- tests/letters.js
+++ tests/letters.js
@@ -54,38 +54,38 @@ QUnit.module('Тестируем функцию letters', function () {
 		assert.strictEqual(letters('hello world', false), 'he world');
 	});
 
-	QUnit.test('Мои тесты на подфункции: Удаление всех повторяющихся букв',function (assert) {
-		assert.strictEqual(deleteAllSameLetters('ЫыЫыЫыЫы'),'');
-		assert.strictEqual(deleteAllSameLetters('Котики и Собачки'),'КтСбач');
-		assert.strictEqual(deleteAllSameLetters('Тесты,опять тесты'),'Т,опяь ');
+	QUnit.test('Мои тесты на подфункции: Удаление всех повторяющихся букв', function (assert) {
+		assert.strictEqual(deleteAllSameLetters('ЫыЫыЫыЫы'), '');
+		assert.strictEqual(deleteAllSameLetters('Котики и Собачки'), 'КтСбач');
+		assert.strictEqual(deleteAllSameLetters('Тесты,опять тесты'), 'Т,опяь ');
 	});
 
-	QUnit.test('Мои тесты на подфункции: Удаление повторок,оставляем первую',function (assert) {
-		assert.strictEqual(deleteAllSameLettersSavingFirst('ЫыЫыЫыЫы'),'Ыы');
-		assert.strictEqual(deleteAllSameLettersSavingFirst('Котики и Собачки'),'Котик Сбач');
-		assert.strictEqual(deleteAllSameLettersSavingFirst('Тесты,опять тесты'),'Тесты,опяь ');
+	QUnit.test('Мои тесты на подфункции: Удаление повторок,оставляем первую', function (assert) {
+		assert.strictEqual(deleteAllSameLettersSavingFirst('ЫыЫыЫыЫы'), 'Ыы');
+		assert.strictEqual(deleteAllSameLettersSavingFirst('Котики и Собачки'), 'Котик Сбач');
+		assert.strictEqual(deleteAllSameLettersSavingFirst('Тесты,опять тесты'), 'Тесты,опяь ');
 	});
 
-	QUnit.test('Мои тесты на подфункции: Удаление повторок,оставляем последнюю',function (assert) {
-		assert.strictEqual(deleteAllSameLettersSavingLast('ЫыЫыЫыЫы'),'Ыы');
-		assert.strictEqual(deleteAllSameLettersSavingLast('Котики и Собачки'),'Кт Собачки');
-		assert.strictEqual(deleteAllSameLettersSavingLast('Тесты,опять тесты'),'Т,опяь есты');
+	QUnit.test('Мои тесты на подфункции: Удаление повторок,оставляем последнюю', function (assert) {
+		assert.strictEqual(deleteAllSameLettersSavingLast('ЫыЫыЫыЫы'), 'Ыы');
+		assert.strictEqual(deleteAllSameLettersSavingLast('Котики и Собачки'), 'Кт Собачки');
+		assert.strictEqual(deleteAllSameLettersSavingLast('Тесты,опять тесты'), 'Т,опяь есты');
 	});
 
-	QUnit.test('Мои тесты:',function (assert) {
-		assert.strictEqual(letters('Бывают огорчения'),'Бывают грчения');
-		assert.strictEqual(letters('Выпей кофе'),'Выпй коф');
-		assert.strictEqual(letters('Поешь булок'),'Пешь булк');
+	QUnit.test('Мои тесты:', function (assert) {
+		assert.strictEqual(letters('Бывают огорчения'), 'Бывают грчения');
+		assert.strictEqual(letters('Выпей кофе'), 'Выпй коф');
+		assert.strictEqual(letters('Поешь булок'), 'Пешь булк');
 
-		assert.strictEqual(letters('Бывают огорчения',true),'Бывают огрчения');
-		assert.strictEqual(letters('Выпей кофе',true),'Выпей коф');
-		assert.strictEqual(letters('Поешь булок',true),'Поешь булк');
+		assert.strictEqual(letters('Бывают огорчения', true), 'Бывают огрчения');
+		assert.strictEqual(letters('Выпей кофе', true), 'Выпей коф');
+		assert.strictEqual(letters('Поешь булок', true), 'Поешь булк');
 
-		assert.strictEqual(letters('Бывают огорчения',false),'Бывают горчения');
-		assert.strictEqual(letters('Выпей кофе',false),'Выпй кофе');
-		assert.strictEqual(letters('Поешь булок',false),'Пешь булок');
+		assert.strictEqual(letters('Бывают огорчения', false), 'Бывают горчения');
+		assert.strictEqual(letters('Выпей кофе', false), 'Выпй кофе');
+		assert.strictEqual(letters('Поешь булок', false), 'Пешь булок');
 
-		assert.strictEqual(letters(''),'');
-		assert.throws(() => { letters()},'Неверный тип');
+		assert.strictEqual(letters(''), '');
+		assert.throws(() => {letters()}, 'Неверный тип');
 	});
 });
